19bcd1d10d9688510fa3bce65fefe27fd2e1814e
zhangnq <248687950@qq.com>
2016-07-22 15:11:09 +0800

    配置文件例子

---
+++ python/aliyun/config-sample.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+access_id='aliyun_testid'
+access_key_secret='aliyun_testkey'
+
+ecs_vpc_id='i-ecsid'
+eip_bandwidth=1
